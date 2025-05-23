d721d188c239249c539dbcbe8bdbba78548704ef
DimasAkbr29 <dimasakbr2777@gmail.com>
2025-05-23 09:58:42 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@ TIMEZONE="Asia/Jakarta"
 #
 KERNEL_NAME="Phoneix"
 KERNEL_ARCH="arm64"
-KERNEL_REPO="https://github.com/DimasAkbr29/Phoneix_android_kernel_xiaomi_mt6768"
+KERNEL_REPO="https://github.com/DimasAkbr29/android_kernel_xiaomi_mt6768"
 KERNEL_BRANCH="lineage-21"
 KERNEL_DEFCONFIG="merlin_defconfig"
 #
